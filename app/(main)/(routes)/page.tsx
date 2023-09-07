99276cdc75d0290a5fec59ee966f434248f1e8bf
Rishabh Singh <rishabhsinghcodes@gmail.com>
2023-09-07 23:16:50 +0530

    initial setup

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+export default function Home() {
+  const state =  true;
+  return (
+    <div>
+      This is a protected route. 
+    </div>
+  )
+}
