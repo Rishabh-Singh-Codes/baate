5f1ecd4271ceea9f89515e207c03a1532a625443
Rishabh Singh <rishabhsinghcodes@gmail.com>
2023-09-08 13:59:10 +0530

    prisma and planetscale initial setup

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,11 +1,15 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   const state =  true;
   return (
     <div>
-      This is a protected route. 
+      <UserButton afterSignOutUrl="/"/>
+      This is a protected route.
+      <ModeToggle />
     </div>
   )
 }
