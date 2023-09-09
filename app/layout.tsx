1b42923a6c14cf79bac7ec24b562879f608caa49
Rishabh Singh <rishabhsinghcodes@gmail.com>
2023-09-09 22:01:56 +0530

    zustand and new server modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,38 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Open_Sans } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
-import { ThemeProvider } from '@/components/providers/theme-provider'
-import { cn } from '@/lib/utils'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
+import { ModalProvider } from "@/components/providers/modal-provider";
 
-const font = Open_Sans({ subsets: ['latin'] })
+const font = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Baate',
-  description: 'Baate, kuch unkahi si, kuch unsuni si, hone lagi.',
-}
+  title: "Baate",
+  description: "Baate, kuch unkahi si, kuch unsuni si, hone lagi.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body className={cn(
-            font.className, 
-            "bg-white dark:bg-[#313338]" 
-          )}>
+        <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
             enableSystem={false}
             storageKey="baate-theme"
           >
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
