961aa1d7035011c5112ef793fde7423cf0b47858
Rishabh Singh <rishabhsinghcodes@gmail.com>
2023-09-11 17:17:12 +0530

    manage members modal

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -60,7 +60,10 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           </DropdownMenuItem>
         )}
         {isModerator && (
-          <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("members", { server })}
+            className="px-3 py-2 text-sm cursor-pointer"
+          >
             Manage Members
             <Users className="h-4 w-4 odd:ml-auto" />
           </DropdownMenuItem>
